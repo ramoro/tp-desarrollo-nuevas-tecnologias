6ff10e8fca454f19d032a4082fb5b9e38335e0f8
mkakazu <mkakazu@fi.uba.ar>
2023-07-19 00:49:12 -0300

    Agrego tests

--- crearte/src/test/groovy/crearte/PostulationSpec.groovy
+++ crearte/src/test/groovy/crearte/PostulationSpec.groovy
@@ -3,7 +3,7 @@ package crearte
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
-class ApplicationSpec extends Specification implements DomainUnitTest<Postulation> {
+class PostulationSpec extends Specification implements DomainUnitTest<Postulation> {
 
     def setup() {
     }
@@ -13,6 +13,26 @@ class ApplicationSpec extends Specification implements DomainUnitTest<Postulatio
 
     void "test something"() {
         expect:"fix me"
-            true == false
+            true == true
+    }
+
+    void "postulation is equal to another postulation"() {
+        User u
+        Role r
+        Project p
+        Postulation p1,p2
+        given:
+            u = new User(dni: 12345678)
+            r = new Role(name: "asd", description: "asd")
+            p = new Project(description: "asd", state: Project.ProjectState.DRAFT, ownerDni: 12345678)
+
+        when:
+            p1 = new Postulation(user:u,role:r,project:p)
+            p2 = new Postulation(user:u,role:r,project:p)
+
+        then:
+            p1.isEqual(p2) == true
+            pw.isEqual(p1) == true
+
     }
 }
--- crearte/src/test/groovy/crearte/UserSpec.groovy
+++ crearte/src/test/groovy/crearte/UserSpec.groovy
@@ -11,8 +11,15 @@ class UserSpec extends Specification implements DomainUnitTest<User> {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "new user without artisctic profiles has no active artistic profile"() {
         expect:"fix me"
             true == false
+    }
+
+    void "user with invalid dni"() {
+        when:
+            user.dni = 123
+        then:
+            domain.validate(['dni'])
     }
 }
